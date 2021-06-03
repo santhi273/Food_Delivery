ad46079a996d4b90901c5ce44904a0307d36376a
venkatesh <himavenkateshk@gmail.com>
2021-06-04 00:42:46 +0530

    IItemServiceAdded

--- src/main/java/com/capg/foodonlinedelivery/service/IItemService.java
+++ src/main/java/com/capg/foodonlinedelivery/service/IItemService.java
@@ -1,5 +1,23 @@
 package com.capg.foodonlinedelivery.service;
 
+import java.util.List;
+
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
+
 public interface IItemService {
+	public Item addItem(Item item);
+
+	public Item updateItem(Item item);
+
+	public Item viewItemById(Integer id);
+
+	public Item removeItem(Item item);
+
+	public List<Item> viewAllItemsByCategory(String name);
+
+	public List<Item> findItemsByRestaurant(String name);
+
+	public List<Item> viewAllItemsByItemName(String name);
+
 
 }
--- src/main/java/com/capg/foodonlinedelivery/service/ItemServiceImplement.java
+++ src/main/java/com/capg/foodonlinedelivery/service/ItemServiceImplement.java
@@ -1,5 +1,51 @@
 package com.capg.foodonlinedelivery.service;
 
+import java.util.List;
+
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
+
 public class ItemServiceImplement implements IItemService {
+
+	@Override
+	public Item addItem(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item updateItem(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item viewItemById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item removeItem(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> viewAllItemsByCategory(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> findItemsByRestaurant(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> viewAllItemsByItemName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
