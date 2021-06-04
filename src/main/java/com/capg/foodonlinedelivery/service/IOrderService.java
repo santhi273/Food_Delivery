38315dbe89279468f186d3abc948377b4b3251f4
SumanthTamanala <sumanthtamanala@gmail.com>
2021-06-04 13:56:39 +0530

    Added Successfully

--- src/main/java/com/capg/foodonlinedelivery/service/IOrderService.java
+++ src/main/java/com/capg/foodonlinedelivery/service/IOrderService.java
@@ -1,5 +1,20 @@
 package com.capg.foodonlinedelivery.service;
 
+import java.util.List;
+
+import com.capg.foodonlinedelivery.entities.OrderDetails;
+
 public interface IOrderService {
+public OrderDetails addOrder(int cartId);
+	
+	public OrderDetails updateOrder(OrderDetails order);
+	
+	public String removeOrderById(OrderDetails order);
+	
+	public OrderDetails viewOrderById(int id);
+	
+	public List<OrderDetails> viewAllOrdersByCustomer(int id);
+	
+	public List<OrderDetails> viewAllOrdersByRestaurant(String resName);
 
 }
--- src/main/java/com/capg/foodonlinedelivery/service/OrderServiceImplement.java
+++ src/main/java/com/capg/foodonlinedelivery/service/OrderServiceImplement.java
@@ -1,5 +1,36 @@
 package com.capg.foodonlinedelivery.service;
 
-public class OrderServiceImplement implements IOrderService {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.capg.foodonlinedelivery.entities.OrderDetails;
+@Service
+@Transactional
+public abstract class OrderServiceImplement implements IOrderService {
+
+	public abstract OrderDetails addOrder(int cartId);
+	{
+
+	}
+		public abstract OrderDetails updateOrder(OrderDetails order);
+		{
+		}
+		
+		
+		public abstract String removeOrderById(OrderDetails order);
+		{
+		}
+		
+		public abstract OrderDetails viewOrderById(int id);{
+		}
+		public abstract List<OrderDetails> viewAllOrdersByCustomer(int id);
+		{
+		}
+		public abstract List<OrderDetails> viewAllOrdersByRestaurant(String resName);
+		{
+		
+	}
 
 }
