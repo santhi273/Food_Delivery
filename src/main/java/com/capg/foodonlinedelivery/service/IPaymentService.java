8baea8564ac281b761888874268bf42379975d2a
hema20699 <chinthakindi.hemalatha@capgemini.com>
2021-06-04 15:56:09 +0530

    Restaurant and payment method declarations added

--- src/main/java/com/capg/foodonlinedelivery/service/IPaymentService.java
+++ src/main/java/com/capg/foodonlinedelivery/service/IPaymentService.java
@@ -1,5 +1,16 @@
 package com.capg.foodonlinedelivery.service;
 
-public interface IPaymentService {
+import java.time.LocalDate;
+import java.util.List;
 
+import com.capg.foodonlinedelivery.entities.Payment;
+
+public interface IPaymentService {
+	public Payment addPayment(Payment payment);
+	public Payment updatePayment(Payment payment);
+	public Payment removePayment(Payment payment);
+	public Payment viewPayment(Payment payment);
+	public List<Payment> viewPaymentByCustomerId(int customerId);
+	public List<Payment> viewPayment(LocalDate startDate,LocalDate endDate);
+	public Payment calculateTotalCost(double payment);
 }
--- src/main/java/com/capg/foodonlinedelivery/service/IRestaurantService.java
+++ src/main/java/com/capg/foodonlinedelivery/service/IRestaurantService.java
@@ -1,5 +1,13 @@
 package com.capg.foodonlinedelivery.service;
 
+import com.capg.foodonlinedelivery.entities.Restaurant;
+
 public interface IRestaurantService {
+	public Restaurant addRestaurant(Restaurant res);
+	public Restaurant updateRestaurant(Restaurant res);
+	public Restaurant removeRestaurant(Restaurant res);
+	public Restaurant viewRestaurant(Restaurant res);
+	public Restaurant viewRestaurantByItemName(String name);
+
 
 }
