17138474c67e28b18930a11a302ccb4b9674cde4
ala santhi <alasanthi98@gmail.com>
2021-06-04 17:53:04 +0530

    Service layer

--- src/main/java/com/capg/foodonlinedelivery/service/ICategoryService.java
+++ src/main/java/com/capg/foodonlinedelivery/service/ICategoryService.java
@@ -7,7 +7,7 @@ import com.capg.foodonlinedelivery.entities.Category;
 public interface ICategoryService {
 public Category addCategory(Category category);
 public Category updateCategory(Category category);
-public Category removeCategory(Category category);
-public Category viewCategory(Category category);
-public List<Category> viewAllCategory(Category category);
+public void removeCategory(Category category);
+public Category viewCategoryById(String categoryId);
+public List<Category> viewAllCategory();
 }
