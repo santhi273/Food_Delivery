434bdd9214ac2a667ee0d77dce2f7fd06f49570e
Mithila <mithila.chenepalli@gmail.com>
2021-06-04 09:34:27 +0530

    Customer service added

--- src/main/java/com/capg/foodonlinedelivery/service/ICustomerService.java
+++ src/main/java/com/capg/foodonlinedelivery/service/ICustomerService.java
@@ -1,5 +1,19 @@
 package com.capg.foodonlinedelivery.service;
 
+import java.util.List;
+
+import com.capg.foodonlinedelivery.entities.Customer;
+
 public interface ICustomerService {
+	
+	public Customer addCustomer(Customer customer);
+	
+	public Customer updateCustomer(Customer customer);
+	
+	public void deleteCustomer();
+	
+	public List<Customer> viewAllCustomers();
+	
+	public Customer ViewCustomer(Customer customer);
 
 }
