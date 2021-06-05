026c22793c8cc1ecd3d108c07cfa696d34a7a93f
ala santhi <alasanthi98@gmail.com>
2021-06-05 10:17:55 +0530

    cart Implementation

--- src/main/java/com/capg/foodonlinedelivery/service/CartServiceImplement.java
+++ src/main/java/com/capg/foodonlinedelivery/service/CartServiceImplement.java
@@ -1,38 +1,81 @@
 package com.capg.foodonlinedelivery.service;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.capg.foodonlinedelivery.entities.FoodCart;
 import com.capg.foodonlinedelivery.entities.Items;
-
+import com.capg.foodonlinedelivery.repository.ICartRepository;
+import com.capg.foodonlinedelivery.repository.IItemRepository;
+@Service
+@Transactional
 public class CartServiceImplement implements ICartService {
-
+	@Autowired
+	ICartRepository repo;
+	@Autowired
+	IItemRepository repo1;
 	@Override
-	public FoodCart additemToCart(FoodCart cart, Items item) {
-		// TODO Auto-generated method stub
-		return null;
+	public FoodCart additemToCart(FoodCart cart,Items item) {
+		cart.getItemList().add(item);
+		return repo.save(cart);
 	}
 
 	@Override
 	public FoodCart increaseQuantity(FoodCart cart, Items item, int quantity) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		int size= cart.getItemList().size();
+		int count=0;
+		for(int i=0;i<size;i++) {
+			String id =cart.getItemList().get(i).getItemId();
+			if(item.getItemId()==id) {
+				count++;
+			}
+		}
+		if(count>0) {
+			for(int i=0;i<quantity;i++) {
+				additemToCart(cart,item);
+			}
+			return cart;
+		}
+		else {
+			return null;
+		}
 	}
 
 	@Override
 	public FoodCart reduceQuantity(FoodCart cart, Items item, int quantity) {
-		// TODO Auto-generated method stub
-		return null;
+		for(int i=0;i<quantity;i++) {
+			removeItem(cart,item);
+		}
+		return cart;
 	}
 
 	@Override
-	public FoodCart removeItem(FoodCart cart, Items item) {
-		// TODO Auto-generated method stub
-		return null;
+	public String removeItem(FoodCart cart, Items item) {
+		int size= cart.getItemList().size();
+		int isPresent=0,index=0;
+		for(int i=0;i<size;i++) {
+			if(item.getItemId()==cart.getItemList().get(i).getItemId()) {
+				isPresent=1;
+				index=i;
+				break;
+			}
+		}
+		if(isPresent==1) {
+			cart.getItemList().remove(index);
+		}
+		cart.setItemList(cart.getItemList());
+		repo.save(cart);
+		return " item removed successfully....";
 	}
 
 	@Override
-	public FoodCart clearCart(FoodCart cart) {
-		// TODO Auto-generated method stub
-		return null;
+	public String clearCart(FoodCart cart) {
+			cart.getItemList().clear();
+			return "cart cleared";
+			
 	}
 
 }
