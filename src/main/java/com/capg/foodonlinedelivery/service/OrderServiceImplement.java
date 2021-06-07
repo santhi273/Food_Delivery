a79d542bf67f2aea39413ca89cf07d23bdb5a297
SumanthTamanala <sumanthtamanala@gmail.com>
2021-06-07 10:11:12 +0530

    Order files Updated Successfully...

--- src/main/java/com/capg/foodonlinedelivery/service/OrderServiceImplement.java
+++ src/main/java/com/capg/foodonlinedelivery/service/OrderServiceImplement.java
@@ -1,16 +1,20 @@
 package com.capg.foodonlinedelivery.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
-import javax.persistence.criteria.Order;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.capg.foodonlinedelivery.entities.FoodCart;
+import com.capg.foodonlinedelivery.entities.Items;
 import com.capg.foodonlinedelivery.entities.OrderDetails;
+import com.capg.foodonlinedelivery.entities.Restaurant;
 import com.capg.foodonlinedelivery.model.OrderDetailsDTO;
+import com.capg.foodonlinedelivery.repository.ICartRepository;
+import com.capg.foodonlinedelivery.repository.IItemRepository;
 import com.capg.foodonlinedelivery.repository.IOrderRepository;
 import com.capg.foodonlinedelivery.utils.OrderDetailsUtils;
 @Service
@@ -18,11 +22,50 @@ import com.capg.foodonlinedelivery.utils.OrderDetailsUtils;
 public abstract class OrderServiceImplement implements IOrderService {
 	@Autowired
 	IOrderRepository repo;
-	public OrderDetailsDTO addOrder(OrderDetails order) {
+	
+	
+	@Autowired
+	ICartRepository repo2;
+	
+	@Autowired
+	IItemRepository repo3;	
+	
+	@Autowired
+	CartServiceImplement service;
+	/*public OrderDetailsDTO addOrder(OrderDetails order) {
 	OrderDetails order1 = repo.save(order);
 		OrderDetailsDTO orderDetailsDto = OrderDetailsUtils.convertToOrderDetailsDto(order1);
 		return orderDetailsDto;
+	}*/
+	public OrderDetailsDTO addOrder(FoodCart cartId) {
+		
+		//logger.info("Inside service add order method");
+		OrderDetails order = new OrderDetails();
+		//OrderDetailsDTO orderDetailsDto = OrderDetailsUtils.convertToOrderDetailsDto(order1);
+		//return orderDetailsDto;
+		//OrderDetailsDTO order=new OrderDetailsDTO();
+		FoodCart cart=repo2.findById(cartId).orElse(null);
+		List<Items> orderList=new ArrayList<Items>();
+		
+		List<Items> item1=cart.getItemList();
+		int list_size=item1.size();
+		for(int i=0;i<list_size;i++)
+		{
+			Items item=item1.get(i);
+			orderList.add(item);
+		}
+		Restaurant rest=item1.get(0).getRestaurant();
+		order.setCustomer(cart.getCustomer());
+		order.setRestaurant(rest);
+		order.setList(orderList);
+		order.setOrderDate(LocalDateTime.now());
+		order.setOrderStatus("Pending");
+		repo.save(order);
+		service.clearCart(cartId);	
+		OrderDetailsDTO orderDetailsDto = OrderDetailsUtils.convertToOrderDetailsDto(order);
+		return orderDetailsDto;
 	}
+
 
 
 	@Override
@@ -58,9 +101,9 @@ public abstract class OrderServiceImplement implements IOrderService {
 
 	@Override
 	public List<OrderDetailsDTO> viewAllOrdersByRestaurant(String restaurantName) {
-		List<OrderDetails> list =repo.findAll();
+		List<OrderDetails> list =repo.findAllByRestaurant(restaurantName);
 		List<OrderDetailsDTO> list1=OrderDetailsUtils.convertToOrderDetailsDtoList(list);
-
+		System.out.println(list);
 		return list1;
 
 	}
